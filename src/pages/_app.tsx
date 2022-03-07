4adf96c4b9835dc3a1d4cdee3015d22c594f9350
renan5g <renansilva.dev5g@gmail.com>
2022-03-07 10:04:44 -0300

    feat: implements ssr styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import type { AppProps } from 'next/app';
+import { globalStyles } from '@stitches';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  globalStyles();
+
   return <Component {...pageProps} />;
 }
 
